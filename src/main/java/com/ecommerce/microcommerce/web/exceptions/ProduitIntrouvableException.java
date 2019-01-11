275b510da4662281d2ebdd067e5f62ec3e4c904e
nicolas cayon-glayere <nicolas.cayon.glayere@gmail.com>
2019-01-11 09:54:27 +0100

    import du projet OK

--- src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitIntrouvableException.java
+++ src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitIntrouvableException.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProduitIntrouvableException extends RuntimeException {
 
-    public ProduitIntrouvableException(String s) {
-        super(s);
-    }
+	private static final long serialVersionUID = 1L;
+
+	public ProduitIntrouvableException(String s) {
+		super(s);
+	}
 }
